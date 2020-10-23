483fc411a3c3ffc97eea38ba2a4a9bff39e0ff1c
Ankur Kunal <mrankur801@gmail.com>
2020-10-23 12:37:16 +0530

    updated created at and updated at feilds

--- src/articles/models.py
+++ src/articles/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
